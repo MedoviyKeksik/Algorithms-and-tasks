76f2fef30ddda389c3309c3c9c0729d0325e9e98
MedoviyKeksik <39442984+MedoviyKeksik@users.noreply.github.com>
2019-07-03 15:39:16 +0300

    Update priority_queue.cpp

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -2,69 +2,48 @@
 
 using namespace std;
 
-const int MAXN = 1e5 + 11;
-int k[MAXN], sz_k;
+struct binary_heap {
+    static const int MAXN = 1e5 + 17;
+    int a[MAXN], sz = 0;
 
-/*
-int sift_up(int i) {
-    while (i != 0 && a[i] > a[(i - 1) / 2]) {
-        swap(a[i], a[(i - 1) / 2]);
-        i = (i - 1) / 2;
-    }
-    return i;
-}
-
-int sift_down(int i) {
-    while (2 * i + 1 < n) {
-        int left = 2 * i + 1, right = 2 * i + 2, j = left;
-        if (right < n && a[right] > a[left]) j = right;
-        if (a[i] > a[j])
-            break;
-        swap(a[i], a[j]);
-        i = j;
-    }
-    return i;
-}
-*/
-
-void in_k(int x) {
-    sz_k++;
-    int i    = sz_k;
-    while (i > 1 && k[i / 2] > x) {
-        k[i] = k[i / 2];
-        i /= 2;
-    }
-    k[i] = x;
-}
-
-void out_k(int &x) {
-    x = k[1];
-    sz_k--;
-    int i = 1;
-    while (i * 2 <= sz_k && k[sz_k + 1] > k[i * 2] || i * 2 + 1 <= sz_k && k[sz_k + 1] > k[i * 2 + 1]) {
-        if (i * 2 + 1 <= sz_k && k[i * 2 + 1] < k[i * 2]) {
-            k[i] = k[i * 2 + 1];
-            i = i * 2 + 1;
-        } else {
-            k[i] = k[i * 2];
-            i = i * 2;
+    void push(int x) {
+        sz++;
+        int i = sz - 1;
+        while (i > 0 && x < a[(i - 1) / 2]) {
+            a[i] = a[(i - 1) / 2];
+            i = (i - 1) / 2;
         }
+        a[i] = x;
     }
-    k[i] = k[sz_k + 1];
-}
+
+    int pop() {
+        int x = a[0];
+        sz--;
+        int i = 0;
+        while ((i * 2 + 1 < sz && a[sz] > a[i * 2 + 1]) || (i * 2 + 2 < sz && a[sz] > a[i * 2 + 2])) {
+            if (i * 2 + 2 < sz && a[i * 2 + 2] < a[i * 2 + 1]) {
+                a[i] = a[i * 2 + 2];
+                i = i * 2 + 2;
+            } else {
+                a[i] = a[i * 2 + 1];
+                i = i * 2 + 1;
+            }
+        }
+        a[i] = a[sz];
+        return x;
+    }
+};
 
 int main() {
-    srand(time(NULL));
+    binary_heap k;
     int n, tmp;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        tmp = rand() % 10000;
-        cout << tmp << ' ';
-        in_k(tmp);
+        cin >> tmp;
+        k.push(tmp);
     }
-    cout << "\n";
-    while (sz_k) {
-        out_k(tmp);
+    for (int i = 0; i < n; i++) {
+        tmp = k.pop();
         cout << tmp << " ";
     }
     return 0;
