b3cc99b38e473b4cf9664c9f5f18812a8015db3f
MedoviyKeksik <39442984+MedoviyKeksik@users.noreply.github.com>
2020-01-20 15:33:34 +0300

    Update Long Arithmetic2.cpp

--- Long Arithmetic2.cpp
+++ Long Arithmetic2.cpp
@@ -1,49 +1,109 @@
-struct BigInt {
-    static const long long BASE = 1e9;
-    static const long long num_base = 9;
-    vector <long long> digits;
+
+class BigInt {
+public:
+    static const int base = 1e9;
+    static const int num_base = 9;
+    vector<int> digits;
+    bool is_neg;
+
+    void remove_leading_zeros() {
+        while (this->digits.size() > 1 && this->digits.back() == 0) this->digits.pop_back();
+        if (this->digits.size() == 1 && this->digits.back() == 0) this->is_neg = false;
+    }
 
     BigInt() {
-        digits.clear();
-        digits.push_back(0);
+        this->is_neg = false;
+        this->digits.push_back(0);
     }
 
     BigInt(long long x) {
-        digits.clear();
-        while (x) {
-            digits.push_back(x % BASE);
-            x /= BASE;
+        if (x < 0) this->is_neg = true;
+        this->digits.push_back(abs(x));
+    }
+
+    BigInt(const string &s) {
+        if (s.size() == 0) {
+            this->is_neg = false;
+            this->digits.push_back(0);
+        } else {
+            this->is_neg = s[0] == '-';
+            for (int i = s.size(); i > 0 + is_neg; i -= num_base) {
+                if (i < num_base + is_neg) {
+                    digits.push_back(atoi(s.substr(0 + is_neg, i).c_str()));
+                } else {
+                    digits.push_back(atoi(s.substr(i - num_base, num_base).c_str()));
+                }
+            }
+            remove_leading_zeros();
         }
     }
 
     BigInt(const BigInt &other) {
-        digits.assign(other.digits.begin(), other.digits.end());
+        *this = other;
     }
 
-    BigInt(string s) {
-        for (int i = s.size(); i > 0; i -= num_base) {
-            if (i < num_base) {
-                digits.push_back(atoi(s.substr(0, i).c_str()));
-            } else {
-                digits.push_back(atoi(s.substr(i - num_base, num_base).c_str()));
+    string to_string() {
+        string res, tmp;
+        if (this->is_neg) res += '-';
+        for (vector<int>::reverse_iterator it = this->digits.rbegin(); it != this->digits.rend(); it++) {
+            tmp = std::to_string(*it);
+            if (it != this->digits.rbegin()) {
+                for (int i = 0; i < num_base - (int) tmp.size(); i++) res += '0';
             }
+            res += tmp;
         }
+        return res;
     }
 
-    BigInt& operator=(const BigInt &other) {
-        digits.assign(other.digits.begin(), other.digits.end());
+    const BigInt& operator+() {
         return *this;
     }
 
+    const BigInt& operator-() {
+        this->is_neg ^= 1;
+        return *this;
+    }
+
+    bool operator== (const BigInt &other) {
+        if (is_neg ^ other.is_neg) {
+            return false;
+        }
+        if (digits.size() != other.digits.size()) return false;
+        for (int i = 0; i < (int) digits.size(); i++) {
+            if (digits[i] != other.digits[i]) return false;
+        }
+        return true;
+    }
+
+
+    bool operator<(const BigInt &other) {
+        if (is_neg ^ other.is_neg) {
+            if (is_neg) return true;
+            else return false;
+        }
+        if (digits.size() < other.digits.size()) return true ^ (is_neg ^ other.is_neg);
+        if (digits.size() > other.digits.size()) return false ^ (is_neg ^ other.is_neg);
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < other.digits[i]) return true ^ (is_neg ^ other.is_neg);
+            if (digits[i] > other.digits[i]) return false ^ (is_neg ^ other.is_neg);
+        }
+        return false;
+    }
+
+    bool operator>(BigInt &other) {
+        return other < *this;
+    }
+
+
     void operator+=(const BigInt &other) {
-        long long carry = 0;
+        long long carry = 0, tmp = 0;
         for (int i = 0; i < (int) max(digits.size(), other.digits.size()) || carry; i++) {
             if (i == (int) digits.size()) {
                 digits.push_back(0);
             }
-            digits[i] += carry + (i < (int) other.digits.size() ?  other.digits[i] : 0);
-            carry = digits[i] / BASE; digits[i] %= BASE;
-            // pr_v(digits);
+            tmp = digits[i] + carry + (i < (int) other.digits.size() ?  other.digits[i] : 0);
+            digits[i] = tmp % base;
+            carry = tmp / base;
         }
     }
 
@@ -58,7 +118,7 @@ struct BigInt {
         for (int i = 0; i < (int) other.digits.size() || carry; i++) {
             digits[i] -= carry + (i < (int) other.digits.size() ? other.digits[i] : 0);
             carry = digits[i] < 0;
-            if (carry) digits[i] += BASE;
+            if (carry) digits[i] += base;
         }
         while (digits.size() > 1 && digits.back() == 0) digits.pop_back();
     }
@@ -69,39 +129,9 @@ struct BigInt {
         return n;
     }
 
-    bool operator== (const BigInt &other) {
-        if (digits.size() != other.digits.size()) return false;
-        for (int i = 0; i < (int) digits.size(); i++) {
-            if (digits[i] != other.digits[i]) return false;
-        }
-        return true;
-    }
-
-    bool operator>(const BigInt &other) {
-        if (digits.size() > other.digits.size()) return true;
-        if (digits.size() < other.digits.size()) return false;
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] > other.digits[i]) return true;
-            if (digits[i] < other.digits[i]) return false;
-        }
-        return false;
-    }
-
-    bool operator<(const BigInt &other) {
-        if (digits.size() < other.digits.size()) return true;
-        if (digits.size() > other.digits.size()) return false;
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] < other.digits[i]) return true;
-            if (digits[i] > other.digits[i]) return false;
-        }
-        return false;
-    }
-
-    void print() {
-        printf("%I64d", digits.back());
-        for (int i = (int) digits.size() - 2; i >= 0; i--) {
-            printf("%09I64d", digits[i]);
-        }
-    }
-
 };
+
+ostream& operator<<(ostream& os, BigInt &a) {
+    os << a.to_string();
+    return os;
+}
