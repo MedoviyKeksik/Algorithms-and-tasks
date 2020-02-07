cabdde0e73ed06ae79b2d3e4195bdb7359a3820e
MedoviyKeksik <39442984+MedoviyKeksik@users.noreply.github.com>
2020-02-07 19:23:11 +0300

    Update Admonds-Karp.cpp

--- Admonds-Karp.cpp
+++ Admonds-Karp.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 const int MAXN = 10;
+vector<vector<int>> f(MAXN, vector<int> (MAXN));
+vector<vector<int>> c(MAXN, vector<int> (MAXN));
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -15,19 +17,17 @@ int main() {
     int n, k;
     cin >> n >> k;
     int u, v, temp;
-    vector<vector<int>> c(MAXN, vector<int> (MAXN));
-    vector<vector<int>> f(MAXN, vector<int> (MAXN));
     for (int i = 0; i < k; i++) {
         cin >> u >> v >> temp;
         u--; v--;
         c[u][v] = temp;
-        c[v][u] = temp;
     }
 
     while (true) {
         vector<int> q(n), from(n, -1);
         int q_sz = 0, id = 0;
         q[q_sz++] = 0;
+        from[0] = 0;
         while (q_sz > id) {
             int now = q[id++];
             for (int to = 0; to < n; to++) {
